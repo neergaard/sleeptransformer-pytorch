26a68ee7a651cbbfecf64641eed9e7b20164a613
aneol <aneol@dtu.dk>
2023-01-29 21:52:13 +0100

    :sparkles:add BaseTransformer class

---
+++ sleeptransformer/models/base_transformer.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+
+
+class BaseTransformer(nn.Module):
+    def __init__(
+        self,
+        n_heads: int,
+        dropout: float,
+        n_layers: int,
+        input_dim: int,
+        hidden_dim: int,
+    ) -> None:
+        super().__init__()
+        # self.n_heads = n_heads
+        # self.dropout = dropout
+        # self.n_layers = n_layers
+        # self.input_dim = input_dim
+        # self.hidden_dim = hidden_dim
+
+        self.encoding_layer = nn.TransformerEncoderLayer(
+            d_model=input_dim,
+            nhead=n_heads,
+            dim_feedforward=hidden_dim,
+            dropout=dropout,
+            batch_first=True,
+        )
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer=self.encoding_layer, num_layers=n_layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+
+        B, T, F = x.shape
+
+        # Positional encoding
+        z = self._positional_embedding(x)
+
+        # run forward pass through transformer
+        z = self.transformer_encoder(z)
+
+        return z
+
+    def _positional_embedding(self, x: torch.Tensor) -> torch.Tensor:
+        p = torch.zeros_like(x).sum(0, keepdim=True)
+        _, T, F = x.shape
+        for j in torch.arange(0, F, dtype=int):
+            if j % 2 == 0:
+                p[0, :, j] = torch.sin(torch.arange(0, T, dtype=int) / 10_000 ** (2 * j / F))
+            else:
+                p[0, :, j] = torch.cos(torch.arange(0, T, dtype=int) / 10_000 ** (2 * j / F))
+
+        return x + p.expand_as(x)
