4eb8e80143ff8ed56fe0e7480a67a918d0ea0bba
Alexander Neergaard Zahid <aneol@dtu.dk>
2023-11-23 15:16:41 +0100

    weight attr is registered properly

--- sleeptransformer/loss/sequence_loss.py
+++ sleeptransformer/loss/sequence_loss.py
@@ -8,8 +8,9 @@ from sleeptransformer.loss.base_loss import BaseLoss
 class SequenceLoss(BaseLoss):
     def __init__(self, weight: Optional[List[float]] = None):
         super().__init__()
-        self.loss_fn = torch.nn.CrossEntropyLoss(weight=torch.tensor(weight) if weight else None, reduction="none")
+        self.register_buffer("weight", torch.tensor(weight) if weight else None)
+        self.loss_fn = torch.nn.CrossEntropyLoss(weight=self.weight, reduction="none")
 
-    def forward(self, y_pred: torch.Tensor, y_target: torch.Tensor) -> torch.Tensor:
-        loss = self.loss_fn(y_pred, y_target)
+    def forward(self, logits: torch.Tensor, y_target: torch.Tensor) -> torch.Tensor:
+        loss = self.loss_fn(logits, y_target)
         return loss
