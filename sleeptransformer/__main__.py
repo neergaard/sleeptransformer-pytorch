40ea8da067d3310c1567f81b65b7e0765f144bc3
aneol <aneol@dtu.dk>
2024-09-29 09:37:38 +0200

    omegaconf as parser apparently not supported by jsonargparse anymore

--- sleeptransformer/__main__.py
+++ sleeptransformer/__main__.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         run=True,
         save_config_kwargs={"overwrite": True},
         parser_kwargs={
-            "parser_mode": "omegaconf",
+            # "parser_mode": "omegaconf",
             "error_handler": None,
             "fit": {"default_config_files": ["sleeptransformer-pytorch/config/sleeptransformer.yaml"]},
         },
@@ -27,6 +27,6 @@ if __name__ == "__main__":
     if cli.subcommand == "fit":
         logger.info("Model fitting complete.")
         logger.info("Running test routine...")
-        cli.trainer.test(dataloaders=cli.datamodule.test_dataloader())
+        cli.trainer.test(dataloaders=cli.datamodule.test_dataloader(), ckpt_path="best")
     if cli.subcommand in ["fit", "test"]:
         logger.info("Test routine finished.")
